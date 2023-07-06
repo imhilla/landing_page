b9e81d61751d5d38064c0c95407e4dddff234570
imhilla <hillaryodhiambo282@gmail.com>
2023-07-06 16:22:49 +0300

    update styles

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -22,7 +22,7 @@ export default function Footer() {
               height: 'auto',
             }}
           />
-          <p className='text-sm lg:text-sm 2xl:text-lg leading-10'>
+          <p className='text-sm lg:text-sm'>
             Get education on business planning,
             direct access to planning experts and a
             funding the valuable platform.
@@ -33,10 +33,10 @@ export default function Footer() {
           <div>
             <h1 className='mb-2 md:mb-4 font-semibold lg:text-xl'>Company</h1>
             <div className='flex flex-col'>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>About Us</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Our Story</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Careers</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Press Center</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>About Us</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Our Story</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Careers</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Press Center</a>
             </div>
           </div>
         </div>
@@ -45,10 +45,10 @@ export default function Footer() {
           <div>
             <h1 className='mb-2 md:mb-4 font-semibold lg:text-xl'>Solution</h1>
             <div className='flex flex-col'>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Customer Support</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Delivery Details</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Terms and Condition</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Works</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Customer Support</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Delivery Details</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Terms and Condition</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Works</a>
             </div>
           </div>
         </div>
@@ -57,10 +57,10 @@ export default function Footer() {
           <div>
             <h1 className='mb-2 md:mb-4 font-semibold lg:text-xl'>Policies</h1>
             <div className='flex flex-col'>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Security Safeguards</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Terms of Service</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Privacy</a>
-              <a className='text-sm mb-2 lg:text-sm	2xl:text-lg hover:underline' href=''>Accessibility</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Security Safeguards</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Terms of Service</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Privacy</a>
+              <a className='text-sm mb-2 lg:text-sm hover:underline' href=''>Accessibility</a>
             </div>
           </div>
         </div>
@@ -68,21 +68,21 @@ export default function Footer() {
         <div className='flex flex-col justify-start md:items-center md:w-1/5 '>
           <div className='flex flex-col justify-between'>
             <h1 className='mb-2 md:mb-4 font-semibold lg:text-xl'>Contact Us</h1>
-            <p className='mb-2 md:mb-4 text-sm lg:text-sm 2xl:text-lg'>You will find your next business quote you prefer.</p>
+            <p className='mb-2 md:mb-4 text-sm lg:text-sm'>You will find your next business quote you prefer.</p>
             <div className='flex flex-row'>
-              <FacebookIcon className='text-4xl mr-2 lg:mr-4' />
-              <InstagramIcon className='text-4xl mr-2 lg:mr-4' />
-              <TwitterIcon className='text-4xl mr-2 lg:mr-4' />
-              <YouTubeIcon className='text-4xl mr-2 lg:mr-4' />
+              <FacebookIcon className='text-3xl mr-2 lg:mr-4' />
+              <InstagramIcon className='text-3xl mr-2 lg:mr-4' />
+              <TwitterIcon className='text-3xl mr-2 lg:mr-4' />
+              <YouTubeIcon className='text-3xl mr-2 lg:mr-4' />
             </div>
           </div>
         </div>
       </div>
 
-      <div className='w-full py-5 md:py-10 flex justify-center border-t	border-t-slate-600'>
+      <div className='w-full py-5 md:py-10 2xl:py-14 flex justify-center border-t	border-t-slate-600'>
         <div className='flex flex-row items-center'>
-          <CopyrightIcon className='mr-1' />
-          <h1 className='text-sm md:text-sm'>2023 Aviera. All rights reserved.</h1>
+          <CopyrightIcon className='mr-1 text-lg' />
+          <h1 className='text-xs md:text-sm'>2023 Aviera. All rights reserved.</h1>
         </div>
       </div>
     </div>
