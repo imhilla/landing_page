24c2e0891b4eaa84790301bffafccda4649fdd95
imhilla <hillaryodhiambo282@gmail.com>
2023-07-07 11:58:48 +0300

    updates

--- src/app/Components/Feature.tsx
+++ src/app/Components/Feature.tsx
@@ -8,7 +8,7 @@ import envantImage from '../../../public/envanto.png'
 
 export default function Feature() {
   return (
-    <div className='w-full flex flex-col items-center justify-center p-10 bg-green-300'>
+    <div className='w-full flex flex-col items-center justify-center px-10 bg-white'>
       <h1 className='font-semibold mb-8 self-center text-2xl md:text-4xl'>We are featured on</h1>
       <div className='w-full flex flex-col items-center justify-center md:flex-row'>
         <div className='w-full flex justify-center items-center'>
