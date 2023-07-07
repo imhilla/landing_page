9fa47e9bcc62b856e4b619cb785412293a112af9
imhilla <hillaryodhiambo282@gmail.com>
2023-07-07 10:26:28 +0300

    updates

--- src/app/Components/ModernConviniences.tsx
+++ src/app/Components/ModernConviniences.tsx
@@ -7,10 +7,10 @@ import CoffeeIcon from '@mui/icons-material/Coffee';
 
 export default function ModernConviniences() {
   return (
-    <div className='min-h-screen bg-white px-10 md:py-28 md:px-20 flex flex-col justify-center items-center w-full'>
-      <h1 className='font-semibold mb-4 md:w-1/2 text-center text-2xl md:text-4xl'>The modern conviniences of this co-working place</h1>
+    <div className='min-h-screen bg-white p-10 md:p-20 lg:p-20 2xl:p-40 flex flex-col justify-center items-center w-full'>
+      <h1 className='font-semibold mb-4 md:w-2/3 text-center text-2xl md:text-4xl'>The modern conviniences of this co-working place</h1>
       <div className='w-full grid gap-5 grid-cols-1 md:flex mt-4'>
-        <div className='w-full bg-slate-100 rounded p-10  flex flex-col justify-center items-center shadow-md hover:bg-white'>
+        <div className='w-full md:w-4/5 bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-lg hover:bg-white transition duration-150 ease-in-out'>
           <span className='my-6'>
             <WifiIcon className='text-4xl' />
           </span>
@@ -21,7 +21,7 @@ export default function ModernConviniences() {
           </p>
         </div>
 
-        <div className='w-full bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-md hover:bg-white'>
+        <div className='w-full md:w-4/5 bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-lg hover:bg-white transition duration-150 ease-in-out'>
           <span className='my-6'>
             <AccessTimeIcon className='text-4xl' />
           </span>
@@ -32,7 +32,7 @@ export default function ModernConviniences() {
           </p>
         </div>
 
-        <div className='w-full bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-md hover:bg-white'>
+        <div className='w-full md:w-4/5 bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-lg hover:bg-white transition duration-150 ease-in-out'>
           <span className='my-6'>
             <LocationOnIcon className='text-4xl' />
           </span>
@@ -43,7 +43,7 @@ export default function ModernConviniences() {
           </p>
         </div>
 
-        <div className='w-full bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-md hover:bg-white'>
+        <div className='w-full md:w-4/5 bg-slate-100 rounded p-10 flex flex-col justify-center items-center shadow-lg hover:bg-white transition duration-150 ease-in-out'>
           <span className='my-6'>
             <CoffeeIcon className='text-4xl' />
           </span>
